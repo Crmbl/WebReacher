752a225eb96ecf889e198cbf75bb6966099f1c6a
Crmbl <axelsch@hotmail.fr>
2018-06-03 22:42:31 +0200

    Final commit, replace iframe.src and go !

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,6 @@
-var isEnabled = true;
-
 function click() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {"enable" : isEnabled}, function(msg) {
-            if (msg == "success")
-                isEnabled = !isEnabled;
-        });
+        chrome.tabs.sendMessage(tabs[0].id, "enable");
     });
 }
 
--- src/content_script.ts
+++ src/content_script.ts
@@ -1,38 +1,83 @@
+window.addEventListener('resize', function() {
+    var canvas = <HTMLCanvasElement> document.getElementById('webreacher-canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    // if (msg.color) {
-    //     console.log('Receive color = ' + msg.color);
-    //     document.body.style.backgroundColor = msg.color;
-    //     sendResponse('Change color to ' + msg.color);
-    // } else {
-    //     sendResponse('Color message is none.');
-    // }
-
-    // if (msg == "createOverlay") {
-    //     createOverlay();
-    //     sendResponse("success");
-    // }
-
-    if (msg.enable != undefined) {
-        if (msg.enable == true) {
-            createOverlay();
-            document.body.style.overflow = "hidden";
+    if (msg == "enable") {
+        if (document.getElementById("webreacher-ssh") == null) {
+            createWebReacherDivs();
         } else {
-            removeOverlay();
-            document.body.style.overflow = null;
+            removeWebReacherDivs();
         }
-        
-        sendResponse("success");
     }
 });
 
-function createOverlay() {
-    var div = document.createElement("div");
-    div.id = "test";
-    document.body.appendChild(div);
+function createWebReacherDivs() {
+    var ssh = document.createElement("div");
+    ssh.id = "webreacher-ssh";
+    ssh.style.marginLeft = window.innerWidth/2 - 300 + "px";
+    ssh.style.marginTop = window.innerHeight/2 - 200 + "px";
+    document.body.appendChild(ssh);
+
+    var handler = document.createElement("div");
+    handler.id = "webreacher-ssh-handler";
+    ssh.appendChild(handler);
+    dragElement(ssh);
+    var separator = document.createElement("hr");
+    separator.id = "webreacher-ssh-handler-separator";
+    handler.appendChild(separator);
+
+    var iframe = document.createElement("iframe");
+    iframe.id = "webreacher-ssh-iframe";
+    iframe.src = "INTERNET WEBSITE !";
+    iframe.width = "375px";
+    iframe.height = "600px";
+    ssh.appendChild(iframe);
 }
 
-function removeOverlay() {
-    var div = document.getElementById("test");
-    document.body.removeChild(div);
+function removeWebReacherDivs() {
+    var ssh = document.getElementById("webreacher-ssh");
+    document.body.removeChild(ssh);
+}
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "-handler")) {
+        document.getElementById(elmnt.id + "-handler").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        elmnt.style.removeProperty('margin-top');
+        elmnt.style.removeProperty('margin-left');
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
